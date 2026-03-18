3682091d29347c0eea2fbaf7d2c75cf6f90c0510
vectorsim <pa05121967@gmail.com>
2026-03-18 11:47:08 +0530

    logo_add
    added png logo

--- examples/rlc_fmu/rlc_fmu.cmd
+++ examples/rlc_fmu/rlc_fmu.cmd
@@ -21,15 +21,8 @@ if "%choice%"=="2" python rlc_fmu_pi_tuning.py
 if "%choice%"=="3" python rlc_fmu_control_training.py
 if "%choice%"=="4" python rlc_fmu_nn_training_animation.py
 if "%choice%"=="5" python rlc_fmu_nn_es.py
-if "%choice%"=="6" goto END
+if "%choice%"=="6" goto exit
 
-REM Pause to see script output
 echo.
 pause
-
-REM Return to menu
 goto MENU
-
-:END
-echo Exiting...
-pause
